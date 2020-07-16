d538829d4610fcec021f3685b3a0390c4509cfd2
EDZ <111>
2020-07-16 11:47:20 +0800

    初次提交

---
+++ src/main/java/com/tacbin/pro/react/FrameworkReactApplication.java
@@ -0,0 +1,14 @@
+package com.tacbin.pro.react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class FrameworkReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrameworkReactApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tacbin/pro/react/FrameworkReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tacbin.pro.react;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameworkReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
